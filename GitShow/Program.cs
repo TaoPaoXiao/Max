32098d989bc4cd010b1af74c4476e7a8694693e8
1419382205@qq.com <1419382205@qq.com>
2021-07-25 08:31:31 +0800

    删库跑路

--- GitShow/Program.cs
+++ GitShow/Program.cs
@@ -1,12 +1,1 @@
-﻿using System;
-
-namespace GitShow
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+﻿
