ab35cb20bb9ae84038c40b02b48772cca43da226
1419382205@qq.com <1419382205@qq.com>
2021-07-25 08:31:58 +0800

    Revert "删库跑路"
    This reverts commit 32098d989bc4cd010b1af74c4476e7a8694693e8.

--- GitShow/Program.cs
+++ GitShow/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using System;
+
+namespace GitShow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
